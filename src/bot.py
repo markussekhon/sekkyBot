5b755564afded77c0e9f41ff7f144cc437e2c123
Markus Sekhon <markus.sekhon@pm.me>
2024-07-17 01:55:22 -0700

    setup postgresql server and framework

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import psycopg2
 import google.generativeai as genai
 
 from dotenv import load_dotenv
@@ -8,6 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 DISCORD_API_KEY = os.getenv('DISCORD_API_KEY')
+DB_NAME = str(os.getenv('POSTGRES_DB'))
+DB_USER = str(os.getenv('POSTGRES_USER'))
+DB_PASSWORD = str(os.getenv('POSTGRES_PASSWORD'))
+DB_HOST = str(os.getenv('POSTGRES_HOST'))
 
 # Initialize gemini
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -17,6 +22,16 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+# Initialize connection to PSQL DB
+dbconn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST
+)
+
+dbcur = dbconn.cursor()
 
 async def probeGemini(prompt):
     response = await model.generate_content_async(
@@ -43,3 +58,6 @@ async def on_message(message):
 
 # Run the client
 client.run(DISCORD_API_KEY)
+
+dbcur.close()
+dbconn.close()
